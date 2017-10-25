d7942c9dc43d1ff835a0ef16b3ac8f6b2a78b50b
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-25 16:42:49 -0700

    this polygon equation sort of works

--- poly.js
+++ poly.js
@@ -19,18 +19,18 @@ context.setTransform(1, 0, 0, 1, 0, 0);
 context.clearRect(0, 0, canvas.width, canvas.height);
 context.translate(350, 350);
 
-var numberOfSides = 6,
-    size = 20,
-    Xcenter = 25,
-    Ycenter = 25;
+var numberOfSides = 5,
+    size = 240,
+    Xcenter = 0,
+    Ycenter = 0;
 
-cxt.beginPath();
-cxt.moveTo (Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));
+context.beginPath();
+context.moveTo (Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));
 
 for (var i = 1; i <= numberOfSides;i += 1) {
-    cxt.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+    context.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
 }
 
-cxt.strokeStyle = "#000000";
-cxt.lineWidth = 1;
-cxt.stroke();
+context.strokeStyle = "#000000";
+context.lineWidth = 1;
+context.stroke();
